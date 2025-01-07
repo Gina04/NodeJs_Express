a406e7ea0647b0254879aa7ce6d52f9378940ee9
Gina04 <gbosque@frba.utn.edu.ar>
2025-01-07 10:45:30 -0300

    thebookphone

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const repl = require('node:repl');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons =[
         { 
           id: 1,
@@ -59,6 +61,31 @@ app.delete('/api/persons/:id', (request, response)=>{
   const id= Number(request.params.id);
   persons= persons.filter(person => person.id !== id)
   response.status(204).end()
+})
+
+const generateId = () =>{
+  const maxId = persons.length > 0
+  ? Math.random(...persons.map(p=> p.id))
+  : 0
+  return maxId + 1;
+}
+
+app.post('/api/persons', (request, response) =>{
+  const body = request.body;
+
+  if(!body.name){
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person)
+  response.json(person)
+
 })
 
   const PORT = 3001

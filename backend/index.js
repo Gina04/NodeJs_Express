22c3fa29b00f66b116e73a321f5ac1f9322ef323
Gina04 <gbosque@frba.utn.edu.ar>
2025-01-18 02:51:59 -0300

    Lint the phonebook, finish

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,32 @@
-const http = require('http');
-const mongoose = require('mongoose');
-const repl = require('node:repl');
-var morgan = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
-const express = require('express');
-const Person = require('./models/person');
+const mongoose = require('mongoose')
+var morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
+const express = require('express')
+const Person = require('./models/person')
 
-const app = express();
+const app = express()
 
 // Configuración de conexión a MongoDB
 const url = process.env.MONGODB_URI
-console.log('connecting to', url);
+console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(result =>{
+  .then(() => {
     console.log('connected to MongoDB')
   })
-  .catch(error =>{
-    console.log('error connecting to MongoDB:', error.message);
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
   })
 
-
-
-
 // Middlewares
-
-morgan.token('body', (req, res) => {
+morgan.token('body', (req) => {
   // Verificar si la solicitud tiene un cuerpo y es JSON
-  return req.method === 'POST' ? JSON.stringify(req.body) : '';
-  
-});
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
 
-app.use(cors());// Habilitar CORS para todas las solicitudes
+})
+
+app.use(cors())// Habilitar CORS para todas las solicitudes
 // Configurar Morgan con el formato 'tiny' y el token 'body'
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
@@ -40,90 +34,90 @@ app.use(
 
 
 // Middleware para procesar JSON
-app.use(express.json());
-app.use(morgan('tiny'));
+app.use(express.json())
+app.use(morgan('tiny'))
 app.use(express.static('dist'))
 
 app.use((req, res, next) => {
   res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; font-src 'self' https://fonts.googleapis.com; style-src 'self' https://fonts.googleapis.com 'unsafe-inline';"
-  );
-  next();
-});
+    'Content-Security-Policy',
+    'default-src \'self\'; font-src \'self\' https://fonts.googleapis.com; style-src \'self\' https://fonts.googleapis.com \'unsafe-inline\';'
+  )
+  next()
+})
 
 // Definición de rutas
 
 /*let persons =[
-        { 
+        {
           id: 1,
-          name: "Arto Hellas", 
+          name: "Arto Hellas",
           number: "040-123456"
         },
-        { 
+        {
           id: 2,
-          name: "Ada Lovelace", 
+          name: "Ada Lovelace",
           number: "39-44-5323523"
         },
-        { 
+        {
           id: 3,
-          name: "Dan Abramov", 
+          name: "Dan Abramov",
           number: "12-43-234345"
         },
-        { 
+        {
           id: 4,
-          name: "Mary Poppendieck", 
+          name: "Mary Poppendieck",
           number: "39-23-6423122"
         }
 ]
 */
 
 
-app.get('/api/persons', (request, response)=>{
-  Person.find({}).then(persons =>{
-    response.json(persons);
-  })  
-  
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+
 })
 
 
-app.get('/info', (request, response) =>{
-    const currentTime = new Date();
-    const numberOfEntries = persons.length;
-    response.send(`
+/*app.get('/info', (request, response) => {
+  const currentTime = new Date()
+  const numberOfEntries = persons.length
+  response.send(`
         <p>Phonebook has info for ${numberOfEntries} people</p>
         <p>${currentTime}</p>`)
 
-});
+})*/
 
-app.get('/api/persons/:id',(request,response, next)=>{
-  Person.findById(request.params.id).then(person =>{
+app.get('/api/persons/:id',(request,response, next) => {
+  Person.findById(request.params.id).then(person => {
     if(person){
       response.json(person)
     }else{
-      response.status(400).end();
+      response.status(400).end()
     }
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 /*app.get('/api/persons/:id',(request, response)=>{
   const id = Number(request.params.id)
   console.log(id)
-  const person = persons.find(person =>person.id === id) 
+  const person = persons.find(person =>person.id === id)
    console.log(person)
 
   if(person){
     response.json(person);
   }else{
     response.status(404).end()
-  }  
+  }
 })*/
 
-app.delete('/api/persons/:id', (request, response, next)=>{
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result =>{
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
     .catch(error => next(error))
   /*const id= Number(request.params.id);
@@ -131,18 +125,18 @@ app.delete('/api/persons/:id', (request, response, next)=>{
   response.status(204).end()*/
 })
 
-const generateId = () =>{
+/*const generateId = () => {
   const maxId = persons.length > 0
-  ? Math.random(...persons.map(p=> p.id))
-  : 0
-  return maxId + 1;
-}
+    ? Math.random(...persons.map(p => p.id))
+    : 0
+  return maxId + 1
+}*/
 
 /*app.post('/api/persons', (request, response) =>{
 
   const body = request.body;
   if(!body.name || !body.number || persons.find(person => person.name === body.name)){
-  
+
     return response.status(400).json({
       error: 'name must be unique'
     })
@@ -158,45 +152,45 @@ const generateId = () =>{
 })*/
 
 app.post('/api/persons', (request, response, next) => {
-  const { name, number } = request.body;
+  const { name, number } = request.body
 
   if (!name || !number) {
-    return response.status(400).json({ error: 'name or number missing' });
+    return response.status(400).json({ error: 'name or number missing' })
   }
 
   const person = new Person({
     name,
     number,
-  });
+  })
 
   person.save().then(savedPerson => {
-    response.json(savedPerson);
+    response.json(savedPerson)
   }).catch(error => {
     //response.status(500).json({ error: 'saving person failed' });
     if(error.name === 'ValidationError'){
-      response.status(400).json({error: error.message});
+      response.status(400).json({ error: error.message })
     }else{
-      next(error);
+      next(error)
     }
-    
-  });
-});
+
+  })
+})
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const {number} = request.body;
+  const { number } = request.body
   // Buscar a la persona por su ID y actualizar el número
-  Person.findByIdAndUpdate(request.params.id, {number}, { new: true, runValidators: true, context: 'query'})
+  Person.findByIdAndUpdate(request.params.id, { number }, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       if (updatedPerson) {
         // Si se encontró la persona y se actualizó correctamente
-        response.json(updatedPerson);
+        response.json(updatedPerson)
       } else {
         // Si no se encuentra la persona con ese ID
-        response.status(404).json({ error: 'Persona no encontrada' });
+        response.status(404).json({ error: 'Persona no encontrada' })
       }
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 
 
@@ -207,7 +201,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if(error.name === 'ValidationError'){
-    return response.status(400).json({error:error.message});
+    return response.status(400).json({ error:error.message })
   }
 
   next(error)
@@ -215,12 +209,11 @@ const errorHandler = (error, request, response, next) => {
 
 
 // Este debe ser el último middleware cargado
-app.use(errorHandler);
+app.use(errorHandler)
 
 // Middleware para manejar rutas no encontradas
 
-  const PORT =process.env.PORT || 3002
-  app.listen(PORT)
-  console.log(`Server running on port ${PORT}`)
+const PORT =process.env.PORT || 3002
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
 
-  
--- backend/models/person.js
+++ backend/models/person.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 /*const password = process.env.MONGODB_URI;*/
-//process.argv[2];
+//process.argv[2]
 
 /* DO NOT SAVE YOUR PASSWORD TO GITHUB!!*/
 const url = process.env.MONGODB_URI
-  //`mongodb+srv://fullstack:${password}@cluster0.eugas.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
+//`mongodb+srv://fullstack:${password}@cluster0.eugas.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
@@ -14,7 +14,7 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
   name:{
     type:String,
-    minLength:3, 
+    minLength:3,
     required: true
   },
   number:{
@@ -22,23 +22,21 @@ const personSchema = new mongoose.Schema({
     validate: {
       validator: function(v) {
         // Expresión regular para validar los formatos de número de teléfono válidos
-        return /^(\d{2,3})-(\d{6,})$/.test(v);
+        return /^(\d{2,3})-(\d{6,})$/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
     },
     required: true
+  }
+})
 
-  } 
-});
-
-const Person = mongoose.model('Person', personSchema);
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
